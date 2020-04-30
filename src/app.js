26bb7e2c42e28afb8b49ec3f55c5a1accd0f17f8
blacmacy <macyleeyijin95@gmail.com>
2020-04-29 23:37:13 -0700

    Add background to the API

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Macy'
+        name: 'Macy Lee'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Macy'
+        name: 'Macy Lee'
     })
 })
 
